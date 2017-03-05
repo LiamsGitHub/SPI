0d6e36df4768d9df36b59433265646509b4e4a62
Liam G <liam@liam.com>
2017-03-05 13:30:25 -0800

    MBP. main.c not working. Move to MM for debug

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 
 void wait (void) {
 	int i=0;
-	for (i=0;i<80000;i++);
+	for (i=0;i<500;i++);
 }
 
 void flash (void)
@@ -15,9 +15,9 @@ void flash (void)
 	int i=0;
 
 	 GPIOC_BSRR=0x2000; // Set PC13
-	 for (i=0;i<0x80000;i++);
+	 for (i=0;i<0x8000;i++);
 	 GPIOC_BSRR=0x20000000; // Clear PC13
-	 for (i=0;i<0x10000;i++);
+	 for (i=0;i<0x8000;i++);
       }
 
 int main(void)
@@ -29,13 +29,14 @@ int main(void)
 {
 
 // Set up power to SPI and allocate pins PA4-7
-// SPI1 is on APB2
+// SPI1 is on APB2, SPI2 is on APB1
 // Also enable GPIO port C for LED
 
-  RCC_APB2ENR= 0x1015; // Start clock to SPI1, GPIO port C (for LED), GPIO port A (for SPI) and AFIO
+	RCC_APB2ENR= 0x101D; // Start clock to SPI1, GPIO port C (for LED), GPIO port B (for SPI2), GPIO port A (for SPI) and AFIO
+	RCC_APB1ENR = 0x4000; // Start clock to SPI2
 
 // Set up GPIO for PC13 to drive LED
-  GPIOC_CRH = 0x200000; 	// PC13 output push pull
+	GPIOC_CRH = 0x200000; 	// PC13 output push pull
   	  	  	  	  	  	  	// GPIO output 10Mhz
 
 // Set up pins for SPI1 on GPIOA_CRL
@@ -44,14 +45,78 @@ int main(void)
 // PA6: MISO. Input. Pull up-down.
 // PA5: SCLK. AF output. 2Mhz
 // PA4. CS. GP Output. 2Mhz
-  GPIOA_CRL = 0xB8B20000;
-  GPIOA_ODR=0x40; // Set PA6 to pull-up input
+  GPIOA_CRL = 0xB8B30000;
+  //GPIOA_ODR=0x40; // Set PA6 to pull-up input
+  GPIOA_ODR=0x0; // See what happens to pull up down
+
+  // Set up pins for SPI2 on GPIOB_CRL
+  // Pins PB12-15 for SPI2
+  // PA7: MOSI. AF output. 50Mhz.
+  // PA6: MISO. Input. Pull up-down.
+  // PA5: SCLK. AF output. 50Mhz
+  // PA4. CS. GP Output. 50Mhz
+
+
+    GPIOB_CRH = 0x8B480000;
+    GPIOB_ODR=0x9000; // Set PA6 to pull-up input
+	//  GPIOB_BSRR=0x1000; // set initial CS state to high
 
 
 // Now set up SPI1
 // SPI_CR1: SSM&SSI set to allow software control of chip select NSS, LSBFirst, SPE enable, Fclk/32 baud rate (250KHz), MASTER, CPOL, CPHA
-  	  SPI_CR1 = 0x367;
+  	  SPI1_CR1 = 0x364;
 
+  	// Now set up SPI2
+  	// SPI2_CR1: SSM&SSI set to allow software control of chip select NSS, LSBFirst, SPE enable, Fclk/32 baud rate (250KHz), SLAVE, CPOL, CPHA
+  	SPI2_CR1 = 0x360;
+
+
+  	  char dataout = 'A';
+  	  char datain=0;
+  	  int i=0;
+
+
+  	  while (1) {
+
+	  // now cs to low
+	  GPIOA_BSRR = 0x100000;
+	  //GPIOB_BSRR = 0x10000000; // CS low
+
+	  while (!(SPI1_SR & 0x2)); // wait for TX buffer to clear
+	  SPI1_DR = dataout;
+
+	  while ((SPI1_SR & 0x80)); // wait while BSY
+	  wait();
+
+
+	  //while (!(SPI2_SR & 0x1)); // SPI2 RXNE
+	  //datain = SPI2_DR;
+
+	  //while ((SPI1_SR & 0x80)); // wait while BSY
+
+	  GPIOA_BSRR=0x10; // set CS hi
+
+
+	  //GPIOB_BSRR=0x1000; // CS hi
+
+
+	  dataout++;
+	  if (dataout=='Z')
+		  dataout='A';
+
+	  flash();
+
+  	  }
+
+}
+
+
+
+
+
+
+
+/*
 
 
 	  // Chip select lo (reset)
@@ -60,7 +125,6 @@ int main(void)
 
 	  //Generate 74+ clock ticks
 	  int i=0;
-	  int status,data=0;
 
 
 	  for (i=0;i<10;i++) {
@@ -79,32 +143,58 @@ int main(void)
 
 // ---- RESET
 
+	  int data[10];
+	  int status[10];
+
+
+	  data[0] = SPI_DR;
+	  status[0] = SPI_SR;
 
 
 	  // send CMD0 (card reset command)
 	  SPI_DR=0x40; // CMD number 0 3. Still overflow. But Rx now empty as I read it.
 	  while (!(SPI_SR & 0x2)); // wait for TX buffer to clear
 
+	  data[1] = SPI_DR;
+	  status[1] = SPI_SR;
+
 	  SPI_DR=0x0; //4
 	  while (!(SPI_SR & 0x2)); // wait for TX buffer to clear
+
+	  data[2] = SPI_DR;
+	  status[2] = SPI_SR;
 
 	  SPI_DR=0x0; //5
 	  while (!(SPI_SR & 0x2)); // wait for TX buffer to clear
 
+	  data[3] = SPI_DR;
+	  status[3] = SPI_SR;
+
 	  SPI_DR=0x0;
 	  while (!(SPI_SR & 0x2)); // wait for TX buffer to clear
+
+	  data[4] = SPI_DR;
+	  status[4] = SPI_SR;
 
 	  SPI_DR=0x0; //6
 	  while (!(SPI_SR & 0x2)); // wait for TX buffer to clear
 
+	  data[5] = SPI_DR;
+	  status[5] = SPI_SR;
+
 	  SPI_DR=0x95; // CRC
 	  while (!(SPI_SR & 0x2)); // wait for TX buffer to clear
-	 // data=SPI_DR;
+
+	  data[6] = SPI_DR;
+	  status[6] = SPI_SR;
 
 	  // ==============
 
 
 	  while (!(SPI_SR & 0x1)); // wait for RXNE RX buffer is ready
+
+	  data[7] = SPI_DR;
+	  status[7] = SPI_SR;
 
 
 
@@ -116,6 +206,8 @@ int main(void)
 
 
 
+	  data[8] = SPI_DR;
+	  status[8] = SPI_SR;
 
 
 
@@ -138,10 +230,8 @@ int main(void)
 		  while (!(SPI_SR & 0x2));
 		  SPI_DR=0xff;
 
-	  }*/
+	  }
 
-	  status=SPI_SR;
-	  data=SPI_DR;
 
 	  while(1);
 
@@ -222,7 +312,7 @@ int main(void)
 
 	  }
 
-*/
+
 
 	  // send CMD17 (Read 512B from address)
 	  SPI_DR=0x51; // CMD17
@@ -253,7 +343,7 @@ int main(void)
 	  flash();
   }
 
-}
+} */
 
 
 
